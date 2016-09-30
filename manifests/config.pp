6cf1e517147b1dde16416b376e44578ae69d53c2
Addi Abel <addi.abel@digital.landregistry.gov.uk>
2016-09-30 12:08:10 +0100

    changed declarations for multiple resources

--- manifests/config.pp
+++ manifests/config.pp
@@ -35,14 +35,14 @@ class nagios::config (
     }
 
   # add another user to the /etc/nagios/cgi.cfg config.
-  file_line { 'cgi.cfg':
+  file_line { 'cgi.conf-1':
     ensure                             => 'present',
     path                               => '/etc/nagios/cgi.cfg',
     line                               => 'authorized_for_all_services=nagiosadmin,nagiosview',
     match                              => '^authorized_for_all_services=',
     }
 
-  file_line { 'cgi.cfg':
+  file_line { 'cgi.conf-2':
     ensure                             => 'present',
     path                               => '/etc/nagios/cgi.cfg',
     line                               => 'authorized_for_all_hosts=nagiosadmin,nagiosview',
@@ -50,7 +50,7 @@ class nagios::config (
     }
 
   # modify the /etc/nagios/nagios.cfg config.
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-1':
     ensure                             => 'absent',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => '#cfg_file=',
@@ -58,119 +58,119 @@ class nagios::config (
     multiple                           => true,
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-2':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'cfg_dir=/etc/nagios/objects',
     match                              => '^cfg_dir=/etc/nagios/conf.d',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-3':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'date_format=strict-iso8601',
     match                              => '^date_format=us',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-4':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'process_performance_data=1',
     match                              => '^process_performance_data=0',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-5':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'host_perfdata_command=process-host-perfdata',
     match                              => '^#host_perfdata_command=process-host-perfdata',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-6':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'service_perfdata_command=process-service-perfdata',
     match                              => '^#service_perfdata_command=process-service-perfdata',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-7':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'host_perfdata_file=/var/log/nagios/host-perfdata',
     match                              => '^#host_perfdata_file=/var/log/nagios/host-perfdata',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-8':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'service_perfdata_file=/var/log/nagios/service-perfdata',
     match                              => '^#service_perfdata_file=/var/log/nagios/service-perfdata',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-9':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'host_perfdata_file_template=[HOSTPERFDATA]\t$TIMET$\t$HOSTNAME$\t$HOSTEXECUTIONTIME$\t$HOSTOUTPUT$\t$HOSTPERFDATA$',
     match                              => '^#host_perfdata_file_template=[HOSTPERFDATA]\t$TIMET$\t$HOSTNAME$\t$HOSTEXECUTIONTIME$\t$HOSTOUTPUT$\t$HOSTPERFDATA$',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-10':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'service_perfdata_file_template=[SERVICEPERFDATA]\t$TIMET$\t$HOSTNAME$\t$SERVICEDESC$\t$SERVICEEXECUTIONTIME$\t$SERVICELATENCY$\t$SERVICEOUTPUT$\t$SERVICEPERFDATA$',
     match                              => '^#service_perfdata_file_template=[SERVICEPERFDATA]\t$TIMET$\t$HOSTNAME$\t$SERVICEDESC$\t$SERVICEEXECUTIONTIME$\t$SERVICELATENCY$\t$SERVICEOUTPUT$\t$SERVICEPERFDATA$',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-11':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'host_perfdata_file_mode=a',
     match                              => '^#host_perfdata_file_mode=a',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-12':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'service_perfdata_file_mode=a',
     match                              => '^#service_perfdata_file_mode=a',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-13':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'host_perfdata_file_processing_interval=15',
     match                              => '^#host_perfdata_file_processing_interval=0',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-14':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'service_perfdata_file_processing_interval=15',
     match                              => '^#service_perfdata_file_processing_interval=0',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-15':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'host_perfdata_file_processing_command=process-host-perfdata-file',
     match                              => '^#host_perfdata_file_processing_command=process-host-perfdata-file',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-16':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'service_perfdata_file_processing_command=process-service-perfdata-file',
     match                              => '^#service_perfdata_file_processing_command=process-service-perfdata-file',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-17':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'host_perfdata_process_empty_results=1',
     match                              => '^#host_perfdata_process_empty_results=1',
     }
 
-  file_line { 'nagios.cfg':
+  file_line { 'nagios.conf-18':
     ensure                             => 'present',
     path                               => '/etc/nagios/nagios.cfg',
     line                               => 'service_perfdata_process_empty_results=1',
