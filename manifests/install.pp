fc87a03a9451228609eaa82bfcab83b36fc6768f
Addi Abel <addi@abel.de.com>
2016-12-07 10:03:46 +0000

    fast-cgi updates

--- manifests/install.pp
+++ manifests/install.pp
@@ -61,14 +61,14 @@ class nagios::install (
         replace => true,
         source  => "puppet:///modules/nagios/spawn-fcgi.redhat",
       } 
-      file { '/usr/lib/systemd/system/fcgiwrap.service.redhat':
+      file { '/usr/lib/systemd/system/fcgiwrap.service':
         ensure  => 'present',
         owner   => 'root',
         group   => 'root',
         mode    => '0755',
         source  => "puppet:///modules/nagios/fcgiwrap.service.redhat",
       } 
-      file { '/usr/lib/systemd/system/fcgiwrap.socket.redhat':
+      file { '/usr/lib/systemd/system/fcgiwrap.socket':
         ensure  => 'present',
         owner   => 'root',
         group   => 'root',
